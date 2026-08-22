75159d4def5c616cb53109b21fb949cbd4b428b6
igor korkin <igor.korkin@gmail.com>
2026-08-23 00:08:41 +0300

    fix macro

--- desktop.ranger.core/security_descriptor.h
+++ desktop.ranger.core/security_descriptor.h
@@ -3,12 +3,6 @@
 #include <memory>
 #include <string>
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
 #include <Windows.h>
 
 class SecurityDescriptor {
