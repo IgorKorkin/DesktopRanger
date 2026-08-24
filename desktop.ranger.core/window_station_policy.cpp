269925c2eea1153d0e5936b6290285ef2fe22b05
igor korkin <igor.korkin@gmail.com>
2026-08-25 00:37:26 +0300

    add WindowStationPolicy, UniqueHandle

---
+++ desktop.ranger.core/window_station_policy.cpp
@@ -0,0 +1,16 @@
+#include "window_station_policy.h"
+
+namespace DesktopRanger::WindowStationPolicy
+{
+	std::expected<UniqueHandle, DWORD> Open(std::wstring_view name) noexcept
+	{
+		UniqueHandle station{ ::OpenWindowStationW(name.data(), FALSE,
+												   READ_CONTROL | WRITE_DAC) };
+
+		if (!station) {
+			return std::unexpected(::GetLastError());
+		}
+
+		return station;
+	}
+} // namespace DesktopRanger::WindowStationPolicy
