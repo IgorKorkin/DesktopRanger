37b222583652cd28082e4fb111d0ae441a678419
igor korkin <igor.korkin@gmail.com>
2026-08-26 22:26:50 +0300

    add UniqueSecurityDescriptor

--- desktop.ranger.core/window_station_policy.cpp
+++ desktop.ranger.core/window_station_policy.cpp
@@ -1,5 +1,7 @@
 #include <string>
 
+#include <Aclapi.h>
+
 #include "window_station_policy.h"
 
 namespace DesktopRanger::WindowStationPolicy
@@ -16,4 +18,20 @@ namespace DesktopRanger::WindowStationPolicy
 
 		return station;
 	}
+
+	std::expected<UniqueSecurityDescriptor, DWORD> SnapshotDacl(HWINSTA station) noexcept
+	{
+		PSECURITY_DESCRIPTOR descriptor{};
+
+		const auto status =
+			::GetSecurityInfo(station, SE_WINDOW_OBJECT, DACL_SECURITY_INFORMATION,
+							  nullptr, nullptr, nullptr, nullptr, &descriptor);
+
+		if (status != ERROR_SUCCESS) {
+			return std::unexpected(status);
+		}
+
+		return UniqueSecurityDescriptor{ descriptor };
+	}
+
 } // namespace DesktopRanger::WindowStationPolicy
--- desktop.ranger.core/window_station_policy.h
+++ desktop.ranger.core/window_station_policy.h
@@ -9,9 +9,16 @@
 
 namespace DesktopRanger::WindowStationPolicy
 {
+
 	using UniqueHandle =
 		wil::unique_any<::HWINSTA, decltype(&::CloseWindowStation), ::CloseWindowStation>;
 
 	[[nodiscard]]
 	std::expected<UniqueHandle, DWORD> Open(std::wstring_view name = L"WinSta0") noexcept;
+
+	using UniqueSecurityDescriptor =
+		wil::unique_any<PSECURITY_DESCRIPTOR, decltype(&::LocalFree), ::LocalFree>;
+
+	[[nodiscard]]
+	std::expected<UniqueSecurityDescriptor, DWORD> SnapshotDacl(HWINSTA station) noexcept;
 } // namespace DesktopRanger::WindowStationPolicy
